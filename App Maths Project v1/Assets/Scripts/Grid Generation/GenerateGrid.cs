5ab36e54f624e462186c9f7131585c0dccff0c1e
Vivienne Johnston <90714025+VivienneTAJ@users.noreply.github.com>
2021-10-26 14:04:00 +0100

    26/10/2021 - Added Grid Generation
    - Implemented grid generation of any valid size
    - Displaying grid still needs fixed (Offset tiles not displayed properly)

---
+++ App Maths Project v1/Assets/Scripts/Grid Generation/GenerateGrid.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateGrid : MonoBehaviour
+{
+    public int gridSize;
+    private int regionWidth, regionHeight;
+    public Tile tilePrefab;
+    public Transform cam;
+    private Tile[,] tiles;
+
+    public GameObject baseRegion;
+    private GameObject[] duplicateRegions;
+
+    void Start()
+    {     
+        GenerateGridInGame();        
+    }
+
+    void GenerateGridInGame()
+    {
+        if (!IsPrime(gridSize) && gridSize > 1)
+        {
+            GetFactors(gridSize);
+            tiles = new Tile[regionWidth, regionHeight];
+            for (int x = 0; x < regionWidth; x++)
+            {
+                for (int z = 0; z < regionHeight; z++)
+                {
+                    var spawnedTile = Instantiate(tilePrefab, new Vector3((x * 10), 0, (z * 10)), Quaternion.identity);
+                    spawnedTile.name = $"Tile {x} {z}";
+                    var isOffset = (x + z) % 2 == 1;
+                    spawnedTile.Init(isOffset);
+                    spawnedTile.transform.parent = baseRegion.transform;
+                    tiles[x, z] = spawnedTile;
+                    cam.transform.position = new Vector3(((float)gridSize / 2 - 0.5f) * 10, (float)gridSize * 10, ((float)gridSize / 2 - 0.5f) * 10);
+                }
+            }
+            duplicateRegions = new GameObject[gridSize];
+            DuplicateRegion();
+        }
+        else
+        {
+            Debug.LogError("Invalid grid size!");
+        }   
+    }
+    bool IsPrime(int gridSize)
+    {
+        if (gridSize == 2 || gridSize == 3)
+            return true;
+        if (gridSize <= 1 || gridSize % 2 == 0 || gridSize % 3 == 0)
+            return false;
+        for (int i = 5; i * i <= gridSize; i += 6)
+        {
+            if (gridSize % i == 0 || gridSize % (i + 2) == 0)
+                return false;
+        }
+        return true;
+    }
+    void GetFactors(int gridSize)
+    {
+        for (int i = 1; i * i <= gridSize; i++)
+        {
+            if (gridSize % i == 0)
+            {
+                regionWidth = gridSize / i;
+                regionHeight = i;
+            }
+        }
+    }
+
+    void DuplicateRegion()
+    {
+        int regionToFollow;
+        int tilesInRow = 1;
+        int tilesInCol = 1;
+
+        duplicateRegions[0] = baseRegion;
+        for (int i = 1; i < gridSize; i++)
+        {            
+            GameObject duplicateRegion = GameObject.Instantiate(baseRegion);
+            duplicateRegions[i] = duplicateRegion;
+            duplicateRegion.transform.parent = transform;
+
+            if (regionWidth < regionHeight)
+            {
+                regionToFollow = regionWidth;
+            }
+            else
+            {
+                regionToFollow = regionHeight;
+            }
+            if (tilesInRow < regionToFollow)
+            {
+                duplicateRegions[i].transform.position = new Vector3(duplicateRegions[i - 1].transform.position.x + (regionWidth * 10), 0, duplicateRegions[i - 1].transform.position.z);
+                tilesInRow++;
+            }
+            else
+            {
+                duplicateRegions[i].transform.position = new Vector3(duplicateRegions[0].transform.position.x, 0, duplicateRegions[i - 1].transform.position.z + (regionHeight * 10));
+                tilesInCol++;
+                tilesInRow = 1;
+            }                             
+        }
+    }
+}
---
+++ App Maths Project v1/Assets/Scripts/Grid Generation/Tile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] private Material tileMaterial, tileMaterialOffset, tileMaterialRegion, tileMaterialRegionOffset; //Holds tile materials
+    [SerializeField] private Renderer tileRenderer; //Holds tile renderer
+    //[SerializeField] private GameObject tileHighlight;
+
+    public void Init(bool isOffset/*, bool isOddRegion*/)
+    {
+        tileRenderer.material = isOffset ? tileMaterialOffset : tileMaterial;
+
+
+        //if (isOddRegion) //If odd region, set the tile material to the dark material
+        //{
+        //    tileRenderer.material = isOffset ? tileMaterialRegionOffset : tileMaterialRegion;
+        //}
+        //else //Otherwise, set the tile material to the light material
+        //{
+        //    tileRenderer.material = isOffset ? tileMaterialOffset : tileMaterial;
+        //}
+    }
+}
