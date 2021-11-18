51f9a9969a31151a2a3eefb04505da1edd8aaa07
Vivienne Johnston <90714025+VivienneTAJ@users.noreply.github.com>
2021-11-18 17:31:38 +0000

    18/11/2021 - Backup

--- App Maths Project v2/Assets/Scripts/UI/GenerateGridSlots.cs
+++ App Maths Project v2/Assets/Scripts/UI/GenerateGridSlots.cs
@@ -3,16 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GenerateGridSlots : MonoBehaviour
-{   
+{
     public NumberOfTiles numberOfTiles;
     public GameObject slot;
+    public GameObject item;
+    public Sprite[] itemImages;
+    //public Transform[] items;
     void Start()
     {
+        //items = new Transform[numberOfTiles.generateGrid.gridSize * numberOfTiles.generateGrid.gridSize - numberOfTiles.generateGrid.gridSize];
+
         for (int i = 0; i < numberOfTiles.generateGrid.gridSize - 1; i++)
         {
-            var newSlotInstance = Instantiate(slot, new Vector3(slot.transform.position.x, slot.transform.position.y - 160f, slot.transform.position.z), Quaternion.identity);
+            var newItemInstance = Instantiate(item, new Vector3(slot.transform.position.x, slot.transform.position.y, slot.transform.position.z), Quaternion.identity);
+            newItemInstance.name = $"Item {i + 2}";
+            newItemInstance.transform.SetParent(slot.transform);
+            item = newItemInstance;
+        }
+
+        for (int i = 0; i < numberOfTiles.generateGrid.gridSize - 1; i++)
+        {
+            var newSlotInstance = Instantiate(slot, new Vector3(slot.transform.position.x, slot.transform.position.y, slot.transform.position.z), Quaternion.identity);
             newSlotInstance.name = $"Slot {i + 2}";
             newSlotInstance.transform.SetParent(transform);
+
+            //foreach (Transform child in newSlotInstance.transform.GetComponentsInChildren<Transform>())
+            //{
+            //    if (child.gameObject.tag == "Item")
+            //    {
+            //        items[i] = child;
+            //    }
+            //}
+
             slot = newSlotInstance;
         }
     }
--- App Maths Project v2/Assets/Scripts/UI/GridSlot.cs
+++ App Maths Project v2/Assets/Scripts/UI/GridSlot.cs
@@ -4,30 +4,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using TMPro;
 
-public class GridSlot : MonoBehaviour, IDropHandler
+public class GridSlot : MonoBehaviour
 {
     public NumberOfTiles numberOfTiles;
-    public GameObject item;
-    private void Start()
-    {        
-        for (int i = 0; i < numberOfTiles.generateGrid.gridSize - 1; i++)
-        {
-            var newItem = Instantiate(item, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
-            newItem.name = $"Item {i + 2}";
-            newItem.transform.SetParent(transform);
-        }       
-    }
-    public void OnDrop(PointerEventData eventData)
-    {
-        Debug.Log("OnDrop");
-        if (eventData.pointerDrag != null)
-        {
-            eventData.pointerDrag.GetComponent<RectTransform>().position = GetComponent<RectTransform>().position;
-            if (numberOfTiles.curNoOfTiles < numberOfTiles.generateGrid.gridSize)
-            {
-                numberOfTiles.curNoOfTiles++;
-                numberOfTiles.numberText.text = $"x{numberOfTiles.curNoOfTiles}";
-            }
-        }       
-    }
 }
